c213e78b214a86d3a6f659239b560e34e6b77340
riteme <riteme@qq.com>
2019-10-16 15:21:36 +0800

    Reformating

--- sll/点双连通分量.cpp
+++ sll/点双连通分量.cpp
@@ -4,19 +4,19 @@
  * description: /dev/null
  */
 void tarjan(ll u) {
-    dfn[u]=low[u]=++tim;
-    sz[u]=1;
-    ll t=0;
-    for(ll i=head[u];i;i=e[i].next) {
-            ll v=e[i].to;
-            if(dfn[v])low[u]=min(low[u],dfn[v]);
-            else {
-                    tarjan(v);
-                    sz[u]+=sz[v];
-                    low[u]=min(low[u],low[v]);
-                    if(dfn[u]<=low[v]) {
-                            ans[u]+=t*sz[v];
-                            t+=sz[v];
-        }}}
-    ans[u]+=t*(n-t-1);
+  dfn[u]=low[u]=++tim;
+  sz[u]=1;
+  ll t=0;
+  for(ll i=head[u];i;i=e[i].next) {
+    ll v=e[i].to;
+    if(dfn[v])low[u]=min(low[u],dfn[v]);
+    else {
+      tarjan(v);
+      sz[u]+=sz[v];
+      low[u]=min(low[u],low[v]);
+      if(dfn[u]<=low[v]) {
+        ans[u]+=t*sz[v];
+        t+=sz[v];
+  }}}
+  ans[u]+=t*(n-t-1);
 }
--- sll/边双连通分量.cpp
+++ sll/边双连通分量.cpp
@@ -4,20 +4,20 @@
  * description: /dev/null
  */
 void tarjan(int x, int f) {
-    dfn[x] = low[x] = ++tot;
-    sta[++tp] = x; vis[x] = true;
-    for (int i = head[x]; i; i = e[i].next) if (i != (f ^ 1)) {
-        if (!dfn[e[i].to]) {
-            tarjan(e[i].to, i);
-            low[x] = min(low[x], low[e[i].to]);
-        } else if (vis[e[i].to])
-            low[x] = min(low[x], dfn[e[i].to]);
-    }
-    if (dfn[x] == low[x]) {
-        scc++;
-        while(1) {
-            int pos = sta[tp--];
-            vis[pos] = false;
-            belong[pos] = scc;
-            if (pos == x) break;
+  dfn[x] = low[x] = ++tot;
+  sta[++tp] = x; vis[x] = true;
+  for (int i = head[x]; i; i = e[i].next) if (i != (f ^ 1)) {
+    if (!dfn[e[i].to]) {
+      tarjan(e[i].to, i);
+      low[x] = min(low[x], low[e[i].to]);
+    } else if (vis[e[i].to])
+      low[x] = min(low[x], dfn[e[i].to]);
+  }
+  if (dfn[x] == low[x]) {
+    scc++;
+    while(1) {
+      int pos = sta[tp--];
+      vis[pos] = false;
+      belong[pos] = scc;
+      if (pos == x) break;
 }}}

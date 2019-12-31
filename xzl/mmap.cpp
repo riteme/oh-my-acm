1ee3a012ba96b5c26f2cb3d08ff11b304437576a
riteme <riteme@qq.com>
2019-12-31 20:04:26 +0800

    Update tabs

--- xzl/mmap.cpp
+++ xzl/mmap.cpp
@@ -8,34 +8,34 @@
 #include <cctype>  // isdigit
 static struct Reader {
 #define BUFSIZE 4194304  // 4MB
-    size_t cur, pos;
-    char *buf;
-    Reader() : cur(BUFSIZE), pos(0), buf(NULL) {}
-    char getc() {
-        if (cur == BUFSIZE) {
-            cur = 0;
-            if (buf)
-                munmap(buf, BUFSIZE);
-            buf = (char*) mmap(
-                NULL, BUFSIZE, PROT_READ, MAP_PRIVATE, STDIN_FILENO, pos
-            );
-            pos += BUFSIZE;
-        }
-        return buf[cur++];
-    }
-    template <typename T>
-    void read(T &x) {
-        char c;
-        x = 0;
-        do c = getc(); while (!isdigit(c));
-        do {
-            x = x * 10 + c - '0';
-            c = getc();
-        } while (isdigit(c));
-    }
-    template <typename T>
-    void operator()(T &x) { read(x); }
-    template <typename T, typename ... TArgs>
-    void operator()(T &x, TArgs& ...args) {
-        read(x); (*this)(args...);
+	size_t cur, pos;
+	char *buf;
+	Reader() : cur(BUFSIZE), pos(0), buf(NULL) {}
+	char getc() {
+		if (cur == BUFSIZE) {
+			cur = 0;
+			if (buf)
+				munmap(buf, BUFSIZE);
+			buf = (char*) mmap(
+				NULL, BUFSIZE, PROT_READ, MAP_PRIVATE, STDIN_FILENO, pos
+			);
+			pos += BUFSIZE;
+		}
+		return buf[cur++];
+	}
+	template <typename T>
+	void read(T &x) {
+		char c;
+		x = 0;
+		do c = getc(); while (!isdigit(c));
+		do {
+			x = x * 10 + c - '0';
+			c = getc();
+		} while (isdigit(c));
+	}
+	template <typename T>
+	void operator()(T &x) { read(x); }
+	template <typename T, typename ... TArgs>
+	void operator()(T &x, TArgs& ...args) {
+		read(x); (*this)(args...);
 }} read;  // read(u, v, w);
